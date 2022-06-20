ce5da7da31229e80535f4bed11306007efc34b86
stonyDeveloper <90153937+stonyDeveloper@users.noreply.github.com>
2022-06-20 07:05:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += "active";
-    setInterval(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
 
